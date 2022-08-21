b68dd3341737a09522e7e881e60eae7e731d7343
petrinaea <kostyleva-katya@mail.ru>
2022-08-21 16:35:19 +0300

    l3_task1 added 3rd task for 1st lesson

---
+++ src/main/java/lesson_1/Apple.java
@@ -0,0 +1,7 @@
+package lesson_1;
+
+public class Apple extends Fruit {
+    public Apple() {
+        super(1.25f);
+    }
+}
--- src/main/java/lesson_1/Main.java
+++ src/main/java/lesson_1/Main.java
@@ -29,4 +29,20 @@ public class Main {
   private static <E> List<E> convertToList(E[] array) {
     return Arrays.asList(array);
   }
+
+  // task3
+  Apple apple1 = new Apple();
+  Apple apple2 = new Apple();
+  Apple apple3 = new Apple();
+
+  Orange orange1 = new Orange();
+  Orange orange2 = new Orange();
+
+  Box<Apple> box1 = new Box<Apple>(apple1, apple2, apple3);
+  Box<Orange> box2 = new Box<Orange>(orange1, orange2);
+
+  System.out.println(box1.compare(box2));
+
+  Box<Orange> box3 = new Box<Orange>();
+        box2.transfer(box3);
 }
